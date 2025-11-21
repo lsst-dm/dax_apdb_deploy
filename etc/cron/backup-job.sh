995af712b74d19844f86b64f2db011ec67d8a4ed
Andy Salnikov <salnikov@slac.stanford.edu>
2025-11-21 13:26:54 -0800

    Make backup cron script more generic

--- etc/cron/backup-job.sh
+++ etc/cron/backup-job.sh
@@ -6,9 +6,10 @@
 usage() {
     cat << EOF
 
-Usage: $0 [options] inventory
+Usage: $0 [options] inventory prefix
 
     inventory is the name of the inventory file.
+    prefix is a prefix for backup name, e.g. "daily", "weekly".
 
 Available options:
 
@@ -32,16 +33,13 @@ while getopts hwl opt; do
 done
 
 shift $(($OPTIND - 1))
-if [ $# -eq 0 ]; then
-    (echo "ERROR: Mising required argument."; usage)>&2
-    exit 1
-fi
-if [ $# -gt 1 ]; then
-    (echo "ERROR: Extra arguments on command line."; usage)>&2
+if [ $# -ne 2 ]; then
+    (echo "ERROR: expecting two positional arguments."; usage)>&2
     exit 1
 fi
 
 inventory="$1"
+prefix="$2"
 
 # Find location of the dax_apdb_deploy.
 cd $(dirname $(dirname $(dirname $(readlink -fn $0))))
@@ -49,10 +47,12 @@ cd $(dirname $(dirname $(dirname $(readlink -fn $0))))
 source setup.sh
 
 # Make backup, do not waitt to finish.
-name="daily-$(date --utc +%Y%m%dT%H%M%SZ)"
+name="${prefix}-$(date --utc +%Y%m%dT%H%M%SZ)"
+echo "Making new backup $name"
 medusa-backup -i "$inventory" make-backup --name="$name" $async
 
 # And list backups.
 if [ $list == "yes" ]; then
+    echo
     medusa-backup -i "$inventory" show-backups
 fi
